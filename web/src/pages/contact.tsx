dc62377c3d89ed3f152488f5bc1535c5abad6817
gledson-ss <glegogle84@gmail.com>
2020-12-23 17:50:12 -0300

    :hammer: chore: add component main

--- web/src/pages/contact.tsx
+++ web/src/pages/contact.tsx
@@ -4,9 +4,20 @@ import * as St from '../styles/pages/contact'
 const Contact: React.FC = () => {
   return (
     <St.Container>
-      <St.topImageContainer>
-        <St.topImage src={'/vectors/Ellipse2.png'} unsized={true} />
-      </St.topImageContainer>
+      <St.TopImageContainer>
+        <St.TopImage src={'/vectors/Ellipse2.png'} unsized={true} />
+      </St.TopImageContainer>
+      <St.ContactMessageContainer>
+        <St.MessageImageContainer>
+          <St.MessageImage src={'/vectors/message.svg'} unsized={true} />
+        </St.MessageImageContainer>
+        <St.MessageContainer>
+          <St.Message>Vamos manter contato!</St.Message>
+        </St.MessageContainer>
+      </St.ContactMessageContainer>
+      <St.ContactMenuContainer>
+        <St.InfoContainer></St.InfoContainer>
+      </St.ContactMenuContainer>
     </St.Container>
   )
 }
--- web/src/styles/pages/contact.ts
+++ web/src/styles/pages/contact.ts
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  padding: 0 100px;
 `
-export const topImageContainer = styled.div`
+export const TopImageContainer = styled.div`
   @media (max-width: 767px) {
     display: none;
   }
@@ -15,7 +17,47 @@ export const topImageContainer = styled.div`
   top: 0px;
   z-index: -1;
 `
-export const topImage = styled(Image)`
-  width: 278px;
-  height: 180px;
+export const TopImage = styled(Image)`
+  width: 378px;
+  height: 200px;
+`
+export const ContactMessageContainer = styled.section`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 18vh;
+  border: 1px solid green;
+`
+export const MessageImageContainer = styled.div`
+  /* border: 1px solid violet; */
+  padding-right: 22px;
+`
+
+export const MessageImage = styled(Image)`
+  width: 100px;
+  height: 80px;
+  /* border: 1px solid red; */
+  margin-left: -10.5px;
+`
+
+export const MessageContainer = styled.div`
+  padding-left: 22px;
+`
+
+export const Message = styled.p`
+  font-size: 36px;
+  @font-face {
+    font-family: proza;
+    src: url('/fonts/proza-libre/ProzaLibre-Regular.ttf');
+  }
+  font-family: proza;
+`
+export const ContactMenuContainer = styled.main`
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: 30% 30% 40%;
+`
+export const InfoContainer = styled.div`
+  display: flex;
+  border: 1px solid red;
 `
