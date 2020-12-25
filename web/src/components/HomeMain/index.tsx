8d153612f31780f8844d857dd5c7a98e52244748
gledson-ss <glegogle84@gmail.com>
2020-12-25 19:09:28 -0300

    :eyeglasses: style: delete line import

--- web/src/components/HomeMain/index.tsx
+++ web/src/components/HomeMain/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 import perfil from '../../assets/photos/perfil.png'
-import vectorHome from '../../assets/vectors/Vector1.png'
 import DescriptionBlock from '../DescriptionBlock'
-import ProjectSeeButton from '../ProjectSeeButton'
 import * as St from './styles'
 
 const HomeMain: React.FC = () => {
