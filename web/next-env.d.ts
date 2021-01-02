bad5a4f463c80530606290aa6b0a220fcb4305fc
gledson-ss <glegogle84@gmail.com>
2021-01-01 21:40:44 -0300

    :eyeglasses: style: lines space

--- web/next-env.d.ts
+++ web/next-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next-images" />
-import { ThemeType } from './src/styles/theme'
 
+import { ThemeType } from './src/styles/theme'
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType
 }
