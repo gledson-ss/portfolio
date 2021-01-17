44c6887961f32b9ef8e0b28a71076f95ed048af9
gledson-ss <glegogle84@gmail.com>
2021-01-17 20:12:26 -0300

    :eyeglasses: style: remove lines

--- web/src/components/DesktopHeader/index.tsx
+++ web/src/components/DesktopHeader/index.tsx
@@ -1,17 +1,11 @@
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import * as St from './styles'
 
 const DesktopHeader: React.FC = () => {
   const router = useRouter()
   const [hasChangePage, setHasChangePage] = useState<boolean>(false)
-  const [currentOption, setCurrentOption] = useState<boolean[]>([
-    false,
-    false,
-    false,
-    false
-  ])
 
   function handleRoute(path: string) {
     router.push(path)
@@ -28,28 +22,16 @@ const DesktopHeader: React.FC = () => {
         <St.Title>gledson.dev</St.Title>
       </St.TitleContainer>
       <St.OptionsContainer>
-        <St.Option
-          actived={currentOption[0]}
-          onClick={() => handleClickAbout('/')}
-        >
+        <St.Option onClick={() => handleClickAbout('/')}>
           <St.DescriptionOption>Home</St.DescriptionOption>
         </St.Option>
-        <St.Option
-          actived={currentOption[1]}
-          onClick={() => handleClickAbout('/projects')}
-        >
+        <St.Option onClick={() => handleClickAbout('/projects')}>
           <St.DescriptionOption>Projetos</St.DescriptionOption>
         </St.Option>
-        <St.Option
-          actived={currentOption[2]}
-          onClick={() => handleClickAbout('/about')}
-        >
+        <St.Option onClick={() => handleClickAbout('/about')}>
           <St.DescriptionOption>Sobre</St.DescriptionOption>
         </St.Option>
-        <St.Option
-          actived={currentOption[3]}
-          onClick={() => handleClickAbout('/contact')}
-        >
+        <St.Option onClick={() => handleClickAbout('/contact')}>
           <St.DescriptionOption>Contato</St.DescriptionOption>
         </St.Option>
       </St.OptionsContainer>
