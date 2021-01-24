95796a28867b8abc67037a614411ec2f5d27a988
gledson-ss <glegogle84@gmail.com>
2021-01-24 19:25:28 -0300

    :eyeglasses: style: unnecessary div remove

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
@@ -9,6 +10,10 @@ import { theme } from '../styles/theme'
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>gledson.dev</title>
+        <link rel="shortcut icon" href="/Icon/gIcon.png" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <GlobalStyle />
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,17 +1,8 @@
-import Head from 'next/head'
 import React from 'react'
 
 import HomeMain from '../components/HomeMain'
 const Home: React.FC = () => {
-  return (
-    <div>
-      <Head>
-        <title>gledson.dev</title>
-        <link rel="shortcut icon" href="/Icon/gIcon.png" />
-      </Head>
-      <HomeMain />
-    </div>
-  )
+  return <HomeMain />
 }
 
 export default Home
